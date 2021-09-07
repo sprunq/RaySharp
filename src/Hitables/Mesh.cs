117c7e381a953d84f03d5db685bbfc8940600e86
Laurin, Tobias <tobias.laurin@stud.hs-kempten.de>
2021-09-07 14:57:02 +0200

    updated mesh construction

--- src/Hitables/Mesh.cs
+++ src/Hitables/Mesh.cs
@@ -14,11 +14,11 @@ namespace Raytracer.Hitables
 {
     public class Mesh : Hitable
     {
+        public int NumberOfFaces;
         private BVHNode _faces;
         private string _objPath;
         private Material _material;
         private double _scale;
-        private Object _lockList = new();
 
         public Mesh() { }
 
@@ -31,7 +31,7 @@ namespace Raytracer.Hitables
             _scale = scale;
             LoadObjFaces();
             buildTime.Stop();
-            Console.WriteLine($"Built Model {objPath} | {buildTime.Elapsed.Seconds}s");
+            Console.WriteLine($"Built Model {objPath} | {((decimal)NumberOfFaces).DynamicPostFix()} | {buildTime.Elapsed.Seconds}s");
         }
 
         public override bool BoundingBox(ref AABB outputBox)
@@ -49,7 +49,8 @@ namespace Raytracer.Hitables
         {
             var objFile = ObjFile.FromFile(_objPath);
             var vertcies = objFile.Vertices;
-            ConcurrentBag<Hitable> faces = new();
+            NumberOfFaces = objFile.Faces.Count();
+            List<Hitable> faces = new();
 
             foreach (var face in objFile.Faces)
             {
@@ -83,55 +84,9 @@ namespace Raytracer.Hitables
                                        _material));
             }
 
-            _faces = new(faces.ToList());
-        }
-
-        public void LoadObjFacesParallel()
-        {
-            var objFile = ObjFile.FromFile(_objPath);
-            var vertcies = objFile.Vertices;
-            ConcurrentBag<Hitable> faces = new();
-
-            Console.WriteLine(objFile.Faces);
-            var chunkedList = objFile.Faces.ChunkTrivialBetter(2 * Environment.ProcessorCount).ToList();
-            Console.WriteLine(chunkedList);
-
-            Parallel.ForEach(chunkedList, chunk =>
-            {
-                foreach (var face in chunk)
-                {
-                    var v0 = face.Vertices[0].Vertex - 1;
-                    var v1 = face.Vertices[1].Vertex - 1;
-                    var v2 = face.Vertices[2].Vertex - 1;
-
-                    var p0 = vertcies[v0].Position;
-                    var p1 = vertcies[v1].Position;
-                    var p2 = vertcies[v2].Position;
-
-                    var normal = new Vector3d(face.Vertices[0].Normal, face.Vertices[1].Normal, face.Vertices[2].Normal);
-
-                    if (face.Vertices.Count == 4)
-                    {
-                        // Face is quad
-                        var v3 = face.Vertices[3].Vertex - 1;
-                        var p3 = vertcies[v3].Position;
-
-                        faces.Add(new Triangle(new Vector3d(p2.X * _scale, p2.Y * _scale, p2.Z * _scale),
-                                               new Vector3d(p3.X * _scale, p3.Y * _scale, p3.Z * _scale),
-                                               new Vector3d(p0.X * _scale, p0.Y * _scale, p0.Z * _scale),
-                                               normal,
-                                               _material));
-                    }
-                    // Face is triangle
-                    faces.Add(new Triangle(new Vector3d(p0.X * _scale, p0.Y * _scale, p0.Z * _scale),
-                                           new Vector3d(p1.X * _scale, p1.Y * _scale, p1.Z * _scale),
-                                           new Vector3d(p2.X * _scale, p2.Y * _scale, p2.Z * _scale),
-                                           normal,
-                                           _material));
-                }
-            });
-
-            _faces = new(faces.ToList());
+            // Most of the time is spent during Construction of the BVH Nodes 
+            // Model with 250k faces: Loading Faces 70ms, BVH Node Construction 7200ms
+            _faces = new(faces);
         }
     }
 }
